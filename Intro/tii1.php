5a18ad61c102a34d2c522d668e24e2ff8bfec35b
Sok Chan Vathana <chanvathana1208@gmail.com>
2025-03-02 12:04:06 +0700

    add

--- Intro/tii1.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    echo "Hello ETEC<br>";
-    echo "Hello ETEC<br>";
-    echo "Hello ETEC<br>";
-    echo "Hello ETEC<br>";
-    echo "Hello ETEC<br>";
-    echo "Hello ETEC<br>";
-    echo "Hello ETEC<br>";
-?>
